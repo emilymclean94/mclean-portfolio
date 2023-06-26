247bae433ae91355eabdb8705694719e51003612
Emily McLean <emilymclean93@gmail.com>
2023-06-26 14:17:12 -0400

    pseudo coded contact page

---
+++ src/app/contact/page.js
@@ -0,0 +1,50 @@
+// export default function Contact() {
+//   return (
+//     <main className="">
+     
+//     </main>
+//   )
+// }
+
+// ! Welcome to the Pseudo Code!
+
+// TODO: Contact Form
+    // ? container <div>
+    // ? div for contents so that it can be flexed
+    // ? heading for form
+    // ? <form> container
+    // ? <div> for form group
+    // ? form <label>
+    // ? form <input>
+
+// TODO: Form Title
+    // ? <h1>
+
+// TODO: Input for name
+    // ? form group <div>
+    // ? label
+    // ? input
+    // ? onMouseOver change cursor
+    // ? onFocus - how does the input box change when clicked on
+
+// TODO: Input for email
+    // ? form group <div>
+    // ? label
+    // ? input
+    // ? onMouseOver change cursor
+    // ? onFocus - how does the input box change when clicked on
+
+// TODO: Input for message
+    // ? form group <div>
+    // ? label
+    // ? input
+    // ? character limit?
+    // ? larger size box
+    // ? onMouseOver change cursor
+    // ? onFocus - how does the input box change when clicked on
+
+// TODO: Submit button
+    // ? onHover slight change to button 
+    // * onSubmit - when form is "submitted" send to email
+    // * refresh page when form is submitted
+

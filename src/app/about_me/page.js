610a3f537515145822a1e2cb2fe4c6e4e15b3221
Emily McLean <emilymclean93@gmail.com>
2023-06-26 11:15:24 -0400

    pseudo coded about me

---
+++ src/app/about_me/page.js
@@ -0,0 +1,35 @@
+// export default function About() {
+//   return (
+//     <main className="">
+     
+//     </main>
+//   )
+// }
+
+// ! Welcome to the Pseudo Code!
+
+// This will use the main layout that has the navbar and footer
+
+// TODO: Main <div> with head shot and about me containers
+    // ! Not sure if I want these as cards or other
+
+// TODO: Head shot <img>
+
+// TODO: About me <div>
+    // ? <div> for heading and paragraphs
+        // ? Heading <h1>
+        // ? 1-2 <p>
+    // ? <div> for technologies
+    // ? <div> for tools
+
+// TODO: Technologies used icons
+    // ? <div> for icons 
+        // ? <ul> ? <ol> ?
+        // ? columns/rows?
+        // ? table?
+
+// TODO: Tools used icons
+    // ? <div> for icons 
+        // ? <ul> ? <ol> ?
+        // ? columns/rows?
+        // ? table?

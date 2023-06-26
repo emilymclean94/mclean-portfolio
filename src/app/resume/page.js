f0baf192569efadafb3b5f4e41616c7da99b2843
Emily McLean <emilymclean93@gmail.com>
2023-06-26 14:28:50 -0400

    pseudo code for resume page

---
+++ src/app/resume/page.js
@@ -0,0 +1,20 @@
+// export default function Resume() {
+//   return (
+//     <main className="">
+     
+//     </main>
+//   )
+// }
+
+// ! Welcome to the Pseudo Code!
+
+// TODO: Resume Container
+    // ? <h1> title
+    // ? <div> for download link
+    // ? <div> for the resume displayed on page?
+
+// TODO: Resume download link
+    // ? Link to "download"
+        // ? href link to google doc
+
+// TODO: Display resume on page?

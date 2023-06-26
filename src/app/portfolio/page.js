54c475370969f6272943b94a2853415f382f7386
Emily McLean <emilymclean93@gmail.com>
2023-06-26 11:46:52 -0400

    pseudo coded portfolio page

---
+++ src/app/portfolio/page.js
@@ -0,0 +1,21 @@
+// export default function Portfolio() {
+//   return (
+//     <main className="">
+     
+//     </main>
+//   )
+// }
+
+// ! Welcome to the Pseudo Code!
+
+// TODO: Project cards
+    // ! not sure if I want an onHover to show links to github/deployed app or just link in description
+    // ? containing <div>
+        // ? <img>
+            // * onHover or onMouseOver? project image display div containing github repo link and deployed app link (both will be icons)
+            // ? <div> with two icons and a semi-opaque background - Z INDEX TO DISPLAY ON TOP. Need absolute positioning??
+                // * href link to github repo
+                // * href link to deployed app
+        // ? info <div>
+            // ? <heading> for title of project
+            // ? <p> for description of project

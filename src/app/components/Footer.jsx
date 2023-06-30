0846aedd1d3b0f21a646e9df8659a8d3be30dc7e
Emily McLean <emilymclean93@gmail.com>
2023-06-30 16:12:03 -0400

    social links now working

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -10,15 +10,22 @@ function Footer() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: "auto", position: "fixed", bottom: 0, left: 0, right: 0 }} 
-    display="flex"
-    justifyContent="center">
-      <BottomNavigation 
-      showLabels
+    <Box
+      sx={{ width: "auto", position: "fixed", bottom: 0, left: 0, right: 0 }}
       display="flex"
-      justifyContent="center">
-        <BottomNavigationAction label="GitHub" icon={<GitHubIcon />} />
-        <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} />
+      justifyContent="center"
+    >
+      <BottomNavigation showLabels display="flex" justifyContent="center">
+        <BottomNavigationAction
+          label="GitHub"
+          icon={<GitHubIcon />}
+          href="https://github.com/emilymclean94"
+        />
+        <BottomNavigationAction
+          label="LinkedIn"
+          icon={<LinkedInIcon />}
+          href="https://www.linkedin.com/in/emily-mclean-2a4896100/"
+        />
       </BottomNavigation>
     </Box>
   );

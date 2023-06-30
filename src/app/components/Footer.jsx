d739ff5e664ead8f3ed7c0e65c32012616e72c40
Emily McLean <emilymclean93@gmail.com>
2023-06-30 16:05:33 -0400

    added material ui bottom nav for gh and linkedin

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,27 @@
+"use client";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 function Footer() {
-    return (
-      <main className="">
-  
-      </main>
-    )
-  }
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Box sx={{ width: "auto", position: "fixed", bottom: 0, left: 0, right: 0 }} 
+    display="flex"
+    justifyContent="center">
+      <BottomNavigation 
+      showLabels
+      display="flex"
+      justifyContent="center">
+        <BottomNavigationAction label="GitHub" icon={<GitHubIcon />} />
+        <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
+
+export default Footer;

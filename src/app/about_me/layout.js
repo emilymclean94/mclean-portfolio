bba04b7f06d3c730bb76fd68eebe9d06a31ec84f
Emily McLean <emilymclean93@gmail.com>
2023-07-03 11:37:58 -0400

    added about bg image and about layout file

---
+++ src/app/about_me/layout.js
@@ -0,0 +1,23 @@
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function AboutLayout({ children }) {
+    return (
+      <html lang="en">
+        <head>
+          <title>Emily McLean</title>
+        </head>
+        <body className='about-bg' sx={{height: 100}}>
+          <header>
+            <NavBar />
+          </header>
+            {children}
+          <footer>
+            <Footer />
+          </footer>
+        </body>
+      </html>
+    )
+  };
+
+  export default AboutLayout;

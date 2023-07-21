4c9fbf1f4b04f1af38cce5e0941ceedc230c9021
Emily McLean <emilymclean93@gmail.com>
2023-07-21 13:26:33 -0400

    adding pseudo code

--- src/app/(business)/portfolio/page.jsx
+++ src/app/(business)/portfolio/page.jsx
@@ -2,7 +2,6 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 import ProjectCards from "@/app/components/ProjectCards";
-// import StreamVerse from "../../../public/stream-verse.png";
 // import Container from "@mui/material/Container";
 
 const styles = {
@@ -37,13 +36,13 @@ export default Portfolio;
 
 // ! Welcome to the Pseudo Code!
 
-// TODO: Project cards
+// Project cards
 // ! not sure if I want an onHover to show links to github/deployed app or just link in description
-// ? containing <div>
-// ? <img>
+//  containing <div>
+//  <img>
 // * onHover or onMouseOver? project image display div containing github repo link and deployed app link (both will be icons)
 // ? <div> with two icons and a semi-opaque background - Z INDEX TO DISPLAY ON TOP. Need absolute positioning??
 // * href link to github repo
 // * href link to deployed app
-// ? <heading> for title of project
-// ? <p> for description of project
+//  <heading> for title of project
+//  <p> for description of project

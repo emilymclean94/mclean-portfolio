a64a43c4e70097bef56d8f63e45b49fa5672b1a3
Emily McLean <emilymclean93@gmail.com>
2023-07-24 19:51:27 -0400

    added breakpoints for mobile

--- src/app/(business)/contact/page.jsx
+++ src/app/(business)/contact/page.jsx
@@ -7,160 +7,171 @@ import { useForm } from "react-hook-form";
 import useWeb3Forms from "@web3forms/react";
 import TextField from "@mui/material/TextField";
 // import Button from "@mui/material/Button";
+import { Grid } from "@mui/material";
+
+const styles = {
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  inputs: {
+    margin: "5px",
+  },
+};
 
 function Contact() {
-    const {
-      register,
-      handleSubmit,
-      reset,
-      watch,
-      control,
-      setValue,
-      formState: { errors, isSubmitSuccessful, isSubmitting },
-    } = useForm({
-      mode: "onTouched",
-    });
-    const [isSuccess, setIsSuccess] = useState(false);
-    const [message, setMessage] = useState(false);
-  
-    // Please update the Access Key in the .env
-    const apiKey = "e8a43124-80fd-4734-bcdb-d3917d21e4fe";
-    // process.env.PUBLIC_ACCESS_KEY ||
-  
-    const { submit: onSubmit } = useWeb3Forms({
-      access_key: apiKey,
-      settings: {
-        from_name: "Portfolio Message",
-        subject: "New Contact Message from your Portfolio Website",
-      },
-      onSuccess: (msg, data) => {
-        setIsSuccess(true);
-        setMessage(msg);
-        reset();
-      },
-      onError: (msg, data) => {
-        setIsSuccess(false);
-        setMessage(msg);
-      },
-    });
-  
-    return (
-      <Box>
-        <Container
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          flexWrap: "wrap",
-          alignContent: "center",
-        }}>
-        <form onSubmit={handleSubmit(onSubmit)} className="">
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    control,
+    setValue,
+    formState: { errors, isSubmitSuccessful, isSubmitting },
+  } = useForm({
+    mode: "onTouched",
+  });
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [message, setMessage] = useState(false);
+
+  // Please update the Access Key in the .env
+  const apiKey = "e8a43124-80fd-4734-bcdb-d3917d21e4fe";
+  // process.env.PUBLIC_ACCESS_KEY ||
+
+  const { submit: onSubmit } = useWeb3Forms({
+    access_key: apiKey,
+    settings: {
+      from_name: "Portfolio Message",
+      subject: "New Contact Message from your Portfolio Website",
+    },
+    onSuccess: (msg, data) => {
+      setIsSuccess(true);
+      setMessage(msg);
+      reset();
+    },
+    onError: (msg, data) => {
+      setIsSuccess(false);
+      setMessage(msg);
+    },
+  });
+
+  return (
+    <Grid
+    container
+      height="100vh"
+      sx={{
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Grid item xs={11} s={6} md={5}>
+        <form
+          style={styles.form}
+          onSubmit={handleSubmit(onSubmit)}
+          className=""
+        >
           <input
             type="checkbox"
             id=""
             className="hidden"
             style={{ display: "none" }}
-            {...register("botcheck")}></input>
-  
-          <div className="">
-            <TextField
-              type="text"
-              placeholder="Full Name"
-              label="Name"
-              autoComplete="false"
-              className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
-                errors.name
-                  ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                  : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-              }`}
-              {...register("name", {
-                required: "Full name is required",
-                maxLength: 80,
-              })}
-            />
-            {errors.name && (
-              <div className="mt-1 text-red-600">
-                <small>{errors.name.message}</small>
-              </div>
-            )}
-          </div>
-  
-          <div className="mb-5">
-            <TextField
-              id="email_address"
-              type="email"
-              placeholder="Email Address"
-              label="Email"
-              name="email"
-              autoComplete="false"
-              className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
-                errors.email
-                  ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                  : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-              }`}
-              {...register("email", {
-                required: "Enter your email",
-                pattern: {
-                  value: /^\S+@\S+$/i,
-                  message: "Please enter a valid email",
-                },
-              })}
-            />
-            {errors.email && (
-              <div className="mt-1 text-red-600">
-                <small>{errors.email.message}</small>
-              </div>
-            )}
-          </div>
-  
-          <div className="mb-3">
-            <TextField
-              name="message"
-              placeholder="Your Message"
-              label="Message"
-              className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white dark:placeholder:text-gray-200 dark:bg-gray-900   rounded-md outline-none  h-36 focus:ring-4  ${
-                errors.message
-                  ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                  : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-              }`}
-              {...register("message", {
-                required: "Enter your Message",
-              })}
-            />
-            {errors.message && (
-              <div className="mt-1 text-red-600">
-                {" "}
-                <small>{errors.message.message}</small>
-              </div>
-            )}
-          </div>
-  
+            {...register("botcheck")}
+          ></input>
+
+          <TextField
+            fullWidth
+            style={styles.inputs}
+            id="outlined-basic"
+            type="text"
+            required
+            label="Full Name"
+            autoComplete="false"
+            {...register("name", {
+              required: "Full name is required",
+              maxLength: 80,
+            })}
+          />
+          {errors.name && (
+            <div className="mt-1 text-red-600">
+              <small>{errors.name.message}</small>
+            </div>
+          )}
+
+          <TextField
+            fullWidth
+            style={styles.inputs}
+            id="outlined-basic"
+            type="email"
+            required
+            label="Email Address"
+            name="email"
+            autoComplete="false"
+            {...register("email", {
+              required: "Enter your email",
+              pattern: {
+                value: /^\S+@\S+$/i,
+                message: "Please enter a valid email",
+              },
+            })}
+          />
+          {errors.email && (
+            <div className="mt-1 text-red-600">
+              <small>{errors.email.message}</small>
+            </div>
+          )}
+
+          <TextField
+            fullWidth
+            style={styles.inputs}
+            id="outlined-multiline-static"
+            multiline
+            required
+            rows={4}
+            name="message"
+            label="Your Message"
+            {...register("message", {
+              required: "Enter your Message",
+            })}
+          />
+          {errors.message && (
+            <div className="mt-1 text-red-600">
+              {" "}
+              <small>{errors.message.message}</small>
+            </div>
+          )}
+
           <button
             type="submit"
-            className="w-full py-4 font-semibold text-white transition-colors bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 px-7 dark:bg-white dark:text-black ">
+            className="w-full py-4 font-semibold text-white transition-colors bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 px-7 dark:bg-white dark:text-black "
+          >
             {isSubmitting ? (
               <svg
                 className="w-5 h-5 mx-auto text-white dark:text-black animate-spin"
-                xmlns="http://www.w3.org/2000/svg" 
+                xmlns="http://www.w3.org/2000/svg"
                 fill="none"
-                viewBox="0 0 24 24">
+                viewBox="0 0 24 24"
+              >
                 <circle
                   className="opacity-25"
                   cx="12"
                   cy="12"
                   r="10"
                   stroke="currentColor"
-                  strokeWidth="4"></circle>
+                  strokeWidth="4"
+                ></circle>
                 <path
                   className="opacity-75"
                   fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
               </svg>
             ) : (
               "Send Message"
             )}
           </button>
         </form>
-  
+
         {isSubmitSuccessful && isSuccess && (
           <div className="mt-3 text-sm text-center text-green-500">
             {message || "Success. Message sent successfully"}
@@ -171,8 +182,8 @@ function Contact() {
             {message || "Something went wrong. Please try later."}
           </div>
         )}
-        </Container>
-      </Box>
+      </Grid>
+    </Grid>
   );
 }
 
@@ -220,7 +231,8 @@ export default Contact;
 // * onSubmit - when form is "submitted" send to email
 // * refresh page when form is submitted
 
-{/* <Box className="">
+{
+  /* <Box className="">
 <Container
   sx={{
     display: "flex",
@@ -235,4 +247,5 @@ export default Contact;
   <TextField id="outlined-basic" label="Message" variant="outlined" />
   <Button variant="outlined">Submit</Button>
 </Container>
-</Box> */}
+</Box> */
+}

7c52d8538091868cb55c4ea40ea4f78ddd7abd06
Emily McLean <emilymclean93@gmail.com>
2023-08-03 12:22:15 -0400

    adding google fonts

--- src/app/(about_me)/layout.jsx
+++ src/app/(about_me)/layout.jsx
@@ -7,6 +7,16 @@ function AboutLayout({ children }) {
     <html lang="en">
       <head>
         <title>Emily McLean</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&family=Montserrat+Alternates:wght@500&family=Secular+One&display=swap"
+          rel="stylesheet"
+        ></link>
       </head>
       <body id="about-bg" sx={{ height: 100 }}>
         <header>

b9dde8db21cadbba7f46e0af0dd90819eb9712bf
Emily McLean <emilymclean93@gmail.com>
2023-09-26 15:10:43 -0400

    updating project descriptions

--- src/app/(business)/portfolio/page.jsx
+++ src/app/(business)/portfolio/page.jsx
@@ -24,7 +24,7 @@ function Portfolio() {
         <ProjectCards
           image="/movie-stock.jpg"
           title="StreamVerse"
-          description="The StreamVerse app allows the user to access, rate, and add movies to different lists (e.g., watched movies and movies to watch). After login, users can view their bio, saved movie list, and to be watched lists on their own dashboard. When a user searches a movie, they can rate it and save it to their watched list or just add it to their want to watch list. Users can add view friends' recent updates by clicking on their profile in in their friend list."
+          description="The StreamVerse app allows users to search, track, and organize movies they've watched by adding them to different lists (e.g., watched movies and movies to watch)."
           github="https://github.com/emilymclean94/stream_verse"
           app="https://streamversee.herokuapp.com/"
         />
@@ -48,7 +48,7 @@ function Portfolio() {
         <ProjectCards
           image="/book-stock.jpg"
           title="Player One Reads"
-          description="This is a simple web application that generates book suggestions based upon the user's video game interests. The app works when a user inputs the title of a video game and may optionally select to narrow down results by category. The app then takes both inputs and returns three book suggestions and links to Google Books."
+          description="This is a simple web application that generates book suggestions based upon the user's video game interests. Users can narrow down results by category."
           github="https://github.com/EGARRISXN/Player-One-Reads"
           app="https://egarrisxn.github.io/Player-One-Reads/"
         />

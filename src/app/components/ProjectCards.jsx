0def6be1e496ddd02c2409173c15b4f24a8e70b6
Emily McLean <emilymclean93@gmail.com>
2023-10-11 16:16:46 -0400

    adjusting font, font size, and colors on project cards

--- src/app/components/ProjectCards.jsx
+++ src/app/components/ProjectCards.jsx
@@ -6,9 +6,9 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 // import { CardActionArea } from "@mui/material";
 import Button from "@mui/material/Button";
-import { FaGithub } from "react-icons/fa";
-import { CgWebsite } from "react-icons/cg";
+import { FaGithub, FaExternalLinkSquareAlt } from "react-icons/fa";
 import Link from "next/link";
+import "../global.css";
 import "../(business)/portfolio/portfolio.css";
 
 const styles = {
@@ -18,39 +18,25 @@ const styles = {
     justifyContent: "center",
     height: "auto",
     width: "auto",
-    backgroundColor: "rgb(66, 63, 50, 0.25)",
-  },
-  screenshotBg: {
-    backgroundColor: "rgb(66, 63, 50, 0.5)",
-  },
-  buttonPosition: {
-    display: "flex",
-    justifyContent: "space-evenly",
-  },
-  links: {
     color: "#c8c2a7",
-    borderColor: "#cdcfc0",
-    border: "solid 3px",
-    fontWeight: "bold",
-    '&:hover': {
-      backgroundColor: "rgb(66, 63, 50, 0.08)" 
-    }
-  },
+  }
 };
 
 function ProjectCards({ image, title, description, github, app }) {
   return (
-    <Card 
-    style={styles.projectCard}>
+    <>
+    <Card style={styles.projectCard}
+    sx={{width: "300px"}}>
       <CardMedia
         component="img"
-        height="300px"
-        width="400px"
+        height="250px"
+        width="300px"
         image={image}
         alt="project screenshot"
-        style={styles.screenshotBg}
+        // sx={{backgroundColor: ""}}
       />
-      <CardContent>
+      <CardContent
+      sx={{backgroundColor: "#423f32"}}>
         <Typography gutterBottom variant="h5" component="div">
           <h2>{title}</h2>
         </Typography>
@@ -60,23 +46,18 @@ function ProjectCards({ image, title, description, github, app }) {
 
         {/* Project Links */}
 
-        <div style={styles.buttonPosition}>
+        <div className="external-icons">
           <Link href={github}>
-            <Button variant="outlined" size="small" style={styles.links}>
-              <FaGithub />
-              Github
-            </Button>
+            <FaGithub size="35" color="#c8c2a7" />
           </Link>
 
           <Link href={app}>
-            <Button variant="outlined" size="small" style={styles.links}>
-              <CgWebsite />
-              Application
-            </Button>
+            <FaExternalLinkSquareAlt size="35" color="#c8c2a7" />
           </Link>
         </div>
       </CardContent>
     </Card>
+    </>
   );
 }
 
